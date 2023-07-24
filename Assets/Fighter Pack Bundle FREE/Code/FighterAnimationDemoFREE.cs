feee5c8153f6f53fe432f7676d166c5a1b7f0c4a
차민호 <71549133+Cha-Minho@users.noreply.github.com>
2023-07-24 13:57:25 +0900

    changed

--- Assets/Fighter Pack Bundle FREE/Code/FighterAnimationDemoFREE.cs
+++ Assets/Fighter Pack Bundle FREE/Code/FighterAnimationDemoFREE.cs
@@ -24,111 +24,49 @@ public class FighterAnimationDemoFREE : MonoBehaviour
 
     void Update()
     {
-        bool wKeyPressed = Input.GetKey(KeyCode.W);
-        bool aKeyPressed = Input.GetKey(KeyCode.A);
-        bool sKeyPressed = Input.GetKey(KeyCode.S);
-        bool dKeyPressed = Input.GetKey(KeyCode.D);
-        bool uKeyPressed = Input.GetKey(KeyCode.U);
-        bool iKeyPressed = Input.GetKey(KeyCode.I);
-        bool kKeyPressed = Input.GetKey(KeyCode.K);
-        bool jKeyPressed = Input.GetKey(KeyCode.J);
+        string animationTrigger = "";
 
-        if (sKeyPressed && dKeyPressed && jKeyPressed)
+        if (Input.GetKey(KeyCode.S))
+            animationTrigger += "s";
+        if (Input.GetKey(KeyCode.D))
+            animationTrigger += "d";
+        if (Input.GetKey(KeyCode.J))
+            animationTrigger += "j";
+        if (Input.GetKey(KeyCode.K))
+            animationTrigger += "k";
+        if (Input.GetKey(KeyCode.U))
+            animationTrigger += "u";
+        if (Input.GetKey(KeyCode.I))
+            animationTrigger += "i";
+        if (Input.GetKey(KeyCode.A))
+            animationTrigger += "a";
+        if (Input.GetKey(KeyCode.W))
+            animationTrigger += "w";
+
+        if (!string.IsNullOrEmpty(animationTrigger))
         {
-            animator.SetTrigger("sdj");
-        }
-        else if (sKeyPressed && dKeyPressed && kKeyPressed)
-        {
-            animator.SetTrigger("sdk");
-        }
-        else if (wKeyPressed && dKeyPressed && uKeyPressed)
-        {
-            animator.SetTrigger("wdu");
-        }
-        else if (sKeyPressed && dKeyPressed && iKeyPressed)
-        {
-            animator.SetTrigger("sdi");
-        }
-        else if (sKeyPressed && dKeyPressed && uKeyPressed)
-        {
-            animator.SetTrigger("sdu");
-        }
-        else if (wKeyPressed && dKeyPressed && iKeyPressed)
-        {
-            animator.SetTrigger("wdi");
-        }
-        else if (sKeyPressed && kKeyPressed)
-        {
-            animator.SetTrigger("sk");
-        }
-        else if (sKeyPressed && jKeyPressed)
-        {
-            animator.SetTrigger("sj");
-        }
-        else if (aKeyPressed)
-        {
-            animator.SetTrigger("a");
-        }
-        else if (kKeyPressed)
-        {
-            animator.SetTrigger("k");
-        }
-        else if (jKeyPressed)
-        {
-            animator.SetTrigger("j");
-        }
-        else if (uKeyPressed)
-        {
-            animator.SetTrigger("u");
-        }
-        else if (dKeyPressed)
-        {
-            animator.SetTrigger("d");
-        }
-        else if (iKeyPressed)
-        {
-            animator.SetTrigger("i");
+            animator.SetTrigger(animationTrigger);
         }
         else
         {
+            animator.ResetTrigger("s");
+            animator.ResetTrigger("d");
+            animator.ResetTrigger("j");
+            animator.ResetTrigger("k");
+            animator.ResetTrigger("u");
+            animator.ResetTrigger("i");
+            animator.ResetTrigger("a");
+            animator.ResetTrigger("w");
             animator.ResetTrigger("sdj");
             animator.ResetTrigger("sdk");
-            animator.ResetTrigger("wdu");
+            animator.ResetTrigger("duw");
             animator.ResetTrigger("sdi");
             animator.ResetTrigger("sdu");
             animator.ResetTrigger("wdi");
             animator.ResetTrigger("sk");
             animator.ResetTrigger("sj");
-            animator.ResetTrigger("a");
-            animator.ResetTrigger("k");
-            animator.ResetTrigger("j");
-            animator.ResetTrigger("u");
-            animator.ResetTrigger("d");
-            animator.ResetTrigger("i");
+
         }
-        //if (Input.GetKeyDown(KeyCode.D))
-        //{
-
-        //    animator.SetBool("move_forward_B", true);
-        //}
-        //else
-        //{
-        //    animator.SetBool("Walk Forward", false);
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    animator.SetBool("Walk Backward", true);
-        //}
-        //else
-        //{
-        //    animator.SetBool("Walk Backward", false);
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.I))
-        //{
-        //    animator.SetTrigger("PunchTrigger");
-        //}
     }
 
     void OnGUI()
