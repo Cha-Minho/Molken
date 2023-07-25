734276487bdd66239e0c6de65cddd4eaf6e41b7d
차민호 <71549133+Cha-Minho@users.noreply.github.com>
2023-07-25 22:53:58 +0900

    box collider

--- Assets/Photon/PhotonUnityNetworking/Code/PhotonNetwork.cs
+++ Assets/Photon/PhotonUnityNetworking/Code/PhotonNetwork.cs
@@ -1847,7 +1847,6 @@ namespace Photon.Pun
             }
             if (NetworkingClient.Server != ServerConnection.MasterServer || !IsConnectedAndReady)
             {
-                Debug.LogError("JoinOrCreateRoom failed. Client is on " + NetworkingClient.Server + " (must be Master Server for matchmaking)" + (IsConnectedAndReady ? " and ready" : "but not ready for operations (State: " + NetworkingClient.State + ")") + ". Wait for callback: OnJoinedLobby or OnConnectedToMaster.");
                 return false;
             }
             if (string.IsNullOrEmpty(roomName))
